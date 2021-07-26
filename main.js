87db23d8db248bc632d15cdf0e7f8a4d662e1537
PRASASTIRAI <77830876+PRASASTIRAI@users.noreply.github.com>
2021-07-26 09:34:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function preload(){}
+
+function setup(){
+    canvas=createCanvas(300,260);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+}
+
+function modelLoaded(){
+console.log("Model Loaded");
+}
+
+function draw(){
+    image(video,0,0,290,250);
+    classifier.classify(video,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+    console.error(error);
+    }
+    else{
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_accuracy_name").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
